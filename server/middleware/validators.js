644fd6b096da6060d2e2816c48bab00361105dc6
Ogidigbo Ejiro <kevinejiro@gmail.com>
2017-10-30 14:20:23 +0100

    add coveralls

--- server/middleware/validators.js
+++ server/middleware/validators.js
@@ -5,11 +5,13 @@
 const validateInput = ({
   name, description, ingredients, directions
 }) => {
-  if (name === undefined || name === '') return { valid: false, message: 'Name field is required.', status: 400 };
-  if (description === undefined || description === '') return { valid: false, message: 'Description field is required.', status: 400 };
-  if (ingredients === undefined || ingredients === '') return { valid: false, message: 'Input your your ingredients.', status: 400 };
-  if (directions === undefined || directions === '') return { valid: false, message: 'Give your users some directions', status: 400 };
-  return { valid: true };
+  let output = '';
+  if (name === undefined || name === '') output = { valid: false, message: 'Name field is required.', status: 400 };
+  if (description === undefined || description === '') output = { valid: false, message: 'Description field is required.', status: 400 };
+  if (ingredients === undefined || ingredients === '') output = { valid: false, message: 'Input your your ingredients.', status: 400 };
+  if (directions === undefined || directions === '') output = { valid: false, message: 'Give your users some directions', status: 400 };
+  output = { valid: true };
+  return output;
 };
 
 export default validateInput;
