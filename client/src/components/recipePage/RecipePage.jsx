d7afcc8950f77102d5dcaf854cb2b0ab03f4be6e
Ejiro k Ogidigbo <kevinejiro@gmail.com>
2018-03-19 15:10:22 +0100

    feature(delete-recipe): add delete recipe functionality
    - add delete recipe action
    
    [Finishes #155945262]

--- client/src/components/recipePage/RecipePage.jsx
+++ client/src/components/recipePage/RecipePage.jsx
@@ -8,136 +8,196 @@ import ReviewForm from './ReviewForm';
 
 // actions
 import getSingleRecipe from '../../actions/getSingleRecipe';
+import voteRecipe from '../../actions/voteRecipe';
+import favouriteRecipe from '../../actions/favouriteRecipe';
+import deleteRecipe from '../../actions/deleteRecipe';
+
+import EditRecipeComponent from './EditRecipeComponent';
+
 /**
+ *
  * @class RecipePage
+ *
+ * @param {string} voteType
  */
 class RecipePage extends React.Component {
   /**
    *
    * @param {object} props
+   *
    */
   constructor(props) {
     super(props);
     this.state = {
-
+      isEditing: false,
     };
+    this.handleVoteBtnClick = this.handleVoteBtnClick.bind(this);
+    this.handleFavouriteBtnClick = this.handleFavouriteBtnClick.bind(this);
+    this.handleDeleteBtnClick = this.handleDeleteBtnClick.bind(this);
   }
-  /**
- * @returns {void} void
- */
-  componentWillMount() {
-    console.log(this.props);
-    this.props.recipe(this.props.match.params.recipeId);
-  }
+
   /**
    * @returns {void} void
    */
   componentDidMount() {
     document.body.classList.add('in-profile');
+    this.props.recipe(this.props.match.params.recipeId);
   }
-  /**
-   *
-   * @param {object} nextProps
-   *
-   * @returns {void} void
-   */
-  componentWillReceiveProps(nextProps) {
-    this.setState({
-    });
-  }
+
   /**
    *@returns {void} void
   */
   componentWillUnmount() {
     document.body.classList.remove('in-profile');
   }
+
+  handleVoteBtnClick = (voteType) => {
+    this.props.voteRecipe(voteType, this.props.oneRecipe.id);
+  }
+  handleFavouriteBtnClick = () => {
+    this.props.favouriteRecipe(this.props.oneRecipe.id);
+  }
+  handleDeleteBtnClick = () => {
+    this.props.deleteRecipe(this.props.oneRecipe.id)
+      .then(() => {
+        this.props.history.push('/dashboard');
+      });
+  }
+  handleEditbtnClick = () => {
+    console.log('this is an edit button click');
+    this.setState({
+      isEditing: true,
+    });
+  }
   /**
    * @returns {JSX} JSX element
    */
   render() {
-    const {
-      recipeTitle,
-      recipeDescription,
-      recipeAuthor,
-      recipeIngredients,
-      recipeDirections,
-    } = this.state;
+    const { isEditing } = this.state;
+    const ID = this.props.oneRecipe.userId;
     return (
       <div>
-        <div className="container marketing layout">
-          <hr className="featurette-divider"
-          />
-          <div className="row featurette">
-            <div className="col-md-5">
-              <img
-                alt="recipe"
-                className="featurette-image img-fluid mx-auto"
-                src="https://res.cloudinary.com/dhgq8vcwi/image/upload/v1519918536/Indomielette.jpg"
-              />
-            </div>
-            <div className="col-md-7">
-              <h2 className="featurette-heading"
-              >
-                {recipeTitle}
-              </h2>
-              <h3>
-                Posted by
-                <Link
-                  href="/dashboard"
-                  to="/dashboard"
-                >
-                  {recipeAuthor}
-                </Link>
-              </h3>
-              <h4 className="page-header">Descriptions</h4>
-              <p>
-                {recipeDescription}
-              </p>
-              <h4 className="page-header">Ingredients</h4>
-              <p>
-                {recipeIngredients}
-              </p>
-              <h4 className="page-header">Directions</h4>
-              <p>
-                {recipeDirections}
-              </p>
-              <div
-                className="icons">
-                <i
-                  aria-hidden="true"
-                  className="fa fa-thumbs-up"
+        {isEditing
+          ? <EditRecipeComponent {...this.props} />
+          : <div className="container marketing layout">
+            <hr className="featurette-divider" />
+            <div className="row featurette">
+              <div className="col-md-5">
+                <img
+                  alt={this.props.oneRecipe.imgUrl}
+                  className="featurette-image img-fluid mx-auto"
+                  src={this.props.oneRecipe.imgUrl}
                 />
-                <i
-                  aria-hidden="true"
-                  className="fa fa-thumbs-down"
-                />
-                <span>
-                  <i
-                    aria-hidden="true"
-                    className="fa fa-trash"
-                  />
-                  <i
-                    aria-hidden="true"
-                    className="fa fa-edit"
-                  />
-                </span>
               </div>
-              <ReviewForm />
-              <ReviewBar />
+              <div className="col-md-7">
+                <h1 className="featurette-heading"
+                >
+                  {this.props.oneRecipe.title}
+                </h1>
+                <h4>
+                  Posted by
+                  &nbsp;
+                          <Link
+                    href={`/user/${ID}`}
+                    to={`/user/${ID}`}
+                  >
+                    {this.props.oneRecipe.User.username}
+                  </Link>
+                </h4>
+                <hr />
+                <h4 className="page-header">Descriptions</h4>
+                <p>
+                  {this.props.oneRecipe.description}
+                </p>
+                <hr />
+                <h4 className="page-header">Ingredients</h4>
+                <p>
+                  {this.props.oneRecipe.ingredients}
+                </p>
+                <hr />
+                <h4 className="page-header">Directions</h4>
+                <p>
+                  {this.props.oneRecipe.direction}
+                </p>
+                <div
+                  className="icons">
+                  <i
+                    aria-hidden="true"
+                    className="fa fa-thumbs-up upvote clickable"
+                    onClick={() => this.handleVoteBtnClick('vote-up')}
+                  >
+                    <span
+                      className="badge badge-default"
+                    >
+                      {this.props.oneRecipe.upvoteCount}
+                    </span>
+                  </i>
+                  <i
+                    aria-hidden="true"
+                    className="fa fa-thumbs-down downvote clickable"
+                    onClick={() => this.handleVoteBtnClick('vote-down')}
+                  >
+                    <span
+                      className="badge"
+                    >
+                      {this.props.oneRecipe.downvoteCount}
+                    </span>
+                  </i>
+                  <i
+                    aria-hidden="true"
+                    className="fa fa-heart clickable favourite"
+                    onClick={this.handleFavouriteBtnClick}
+                  />
+                  {
+                    this.props.authUserId === this.props.oneRecipe.userId &&
+                    <span
+                      className="righticons">
+                      <i
+                        aria-hidden="true"
+                        className="fa fa-trash trash clickable"
+                        onClick={this.handleDeleteBtnClick}
+                      />
+                      <i
+                        aria-hidden="true"
+                        className="fa fa-edit edit clickable"
+                        onClick={this.handleEditbtnClick}
+                      />
+                    </span>
+                  }
+                </div>
+                <ReviewForm recipeId={this.props.oneRecipe.id} />
+                <ReviewBar {...this.props}
+                />
+              </div>
             </div>
           </div>
-        </div>
+        }
       </div >
     );
   }
 }
-
+/**
+ * @param {Object} state
+ *
+ * @returns {Object} new state
+ */
 const mapStateToProps = state => ({
-  getSingleRecipe: state.recipe
+  authUserId: state.auth.user.id,
+  oneRecipe: state.recipes.oneRecipe,
+  userIsAuthenticated: state.auth.isAuthenticated,
 });
 
+/**
+ *
+ * @param {any} dispatch
+ *
+ * @returns {void}
+ */
 const mapDispatchToProps = dispatch => ({
-  recipe: id => dispatch(getSingleRecipe(id))
+  recipe: id => dispatch(getSingleRecipe(id)),
+  voteRecipe: (voteType, id) => dispatch(voteRecipe(voteType, id)),
+  favouriteRecipe: id => dispatch(favouriteRecipe(id)),
+  deleteRecipe: id => dispatch(deleteRecipe(id)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(RecipePage);
--- client/src/components/recipePage/ReviewBar.jsx
+++ client/src/components/recipePage/ReviewBar.jsx
@@ -1,35 +1,82 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import Reviews from './Reviews';
+
+import getAllReviews from '../../actions/getAllReviews';
 
 /**
  *
 */
-const ReviewBar = (props) => (
-  <div>
-    <div
-      className="my-3 p-3 bg-dark rounded box-shadow">
-      <h6
-       className="border-bottom border-gray pb-2 mb-0"
-       >
-       Recent Reviews
-      </h6>
-      {/* <Reviews
-        reviewText={}
-        username={}
-      /> */}
-      <small
-        className="d-block text-right mt-3">
-        <Link
-          href="/dashboard"
-          to="/dashboard"
-        >
-          All Reviews
-        </Link>
-      </small>
-    </div>
-  </div>
-);
+class ReviewBar extends React.Component {
+  /**
+   *
+   * @param {object} props
+   */
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-export default ReviewBar;
+  /**
+   * @returns {void} void
+   */
+  componentWillMount() {
+    this.props.reviews(this.props.match.params.recipeId);
+  }
+
+  /**
+   * @returns {JSX} JSX element
+   */
+  render() {
+    const reviews = this.props.allReviews ? this.props.allReviews : [];
+    let reviewList;
+    if (reviews.length === 0) {
+      reviewList = (
+        <div className="text-center">
+          <h6> This recipe hasnt been reviewed </h6>
+        </div>
+      );
+    } else {
+      reviewList = reviews.map((review, i) => (
+        <div key={`review${i + 1}`}>
+          <Reviews {...review} />
+        </div>
+      ));
+    }
+    return (
+      <div>
+        <div
+          className="my-3 p-3 bg-dark rounded box-shadow">
+          <h6
+            className="border-bottom border-gray pb-2 mb-0"
+          >
+            Recent Reviews
+          </h6>
+          {reviewList}
+        </div>
+      </div>
+    );
+  }
+}
+
+/**
+ * @param {Object} state
+ *
+ * @returns {Object} new state
+ */
+const mapStateToProps = state => ({
+  allReviews: state.recipes.allReviews
+});
+
+/**
+ *
+ * @param {any} dispatch
+ *
+ * @returns {void}
+ */
+const mapDispatchToProps = dispatch => ({
+  reviews: id => dispatch(getAllReviews(id))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(ReviewBar);
